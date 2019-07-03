5fd43518bf03a3edbfe26a497841dda5546ad128
chskela <chskela@mail.ru>
2019-07-03 13:02:43 +0300

    Промежуточное

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-// JavaScript source code
+const url = document.querySelector('input[type="url"]');
+
+const display = document.querySelector('#display');
+const items = document.querySelectorAll('.item');
+const str = 'https://pixabay.com/get/52e2dd474257aa14f6d1867dda6d49214b6ac3e456577348712a79d692/scent-of-jasmine-4284856_1920.jpg';
+
+display.addEventListener('click', () => {
+    
+    imgLoad(str);
+    console.log(url.value);
+})
+
+function imgLoad(url) {
+    items.forEach((item) => {
+        const src = item.querySelector('img').src = url;
+        console.log(src);
+    })
+} 
