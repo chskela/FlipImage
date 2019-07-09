537ce4192d8d1c7af5c8015db4e0ec43f52f69d4
chskela <chskela@mail.ru>
2019-07-09 11:06:33 +0300

    Базовая реализация

--- src/index.js
+++ src/index.js
@@ -2,17 +2,48 @@ const url = document.querySelector('input[type="url"]');
 
 const display = document.querySelector('#display');
 const items = document.querySelectorAll('.item');
-const str = 'https://pixabay.com/get/52e2dd474257aa14f6d1867dda6d49214b6ac3e456577348712a79d692/scent-of-jasmine-4284856_1920.jpg';
+const up = document.querySelectorAll('.up');
+const down = document.querySelectorAll('.down');
+const left = document.querySelectorAll('.left');
+const right = document.querySelectorAll('.right');
+
+const str = '1.jpg';
+
+imgLoad(str);
 
 display.addEventListener('click', () => {
-    
-    imgLoad(str);
-    console.log(url.value);
+    if(url.value === '') {
+      imgLoad(str);
+    } else {
+      imgLoad(url.value)
+    } 
 })
 
+up.forEach((item) => {
+    item.addEventListener('click', (event) => {
+    event.target.parentElement.lastElementChild.style = "transform: scaleY(-1);"
+    })
+})
+
+down.forEach((item) => {
+    item.addEventListener('click', (event) => {
+    event.target.parentElement.lastElementChild.style = "transform: scaleY(1);"
+    })
+})
+
+left.forEach((item) => {
+  item.addEventListener('click', (event) => {
+    event.target.parentElement.lastElementChild.style = "transform: scaleX(1);"
+  })
+})
+
+right.forEach((item) => {
+  item.addEventListener('click', (event) => {
+    event.target.parentElement.lastElementChild.style = "transform: scaleX(-1);"
+  })
+})
 function imgLoad(url) {
     items.forEach((item) => {
         const src = item.querySelector('img').src = url;
-        console.log(src);
     })
 } 
